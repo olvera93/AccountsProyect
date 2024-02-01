ffb11d5ebfbf13be4cfd11f9d62cbd8c8d84dab8
olvera93 <olvera9302@outlook.com>
2024-01-31 18:12:37 -0600

    [ADD] Swagger

--- src/main/java/com/olvera/accounts/dto/AccountsDto.java
+++ src/main/java/com/olvera/accounts/dto/AccountsDto.java
@@ -1,19 +1,27 @@
 package com.olvera.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Accounts information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "AccountName can not be a null or empty")
     @Pattern(regexp = "($|[0-9]{10})", message = "AccountNumber must be 10 digits")
+    @Schema(description = "Account Number of Eazy Bank account")
     private Long accountNumber;
 
+    @Schema(description = "Account Type of Eazy Bank account", example = "Savings")
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @Schema(description = "Eazy Bank branch address")
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 
